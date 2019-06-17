eea5124e76e122f78cb5a716007280bf68c83761
Jarana Manotumruksa <feay1234@gmail.com>
2019-06-17 11:33:55 +0100

    update script

--- data/getData.sh
+++ data/getData.sh
@@ -2,3 +2,7 @@ wget http://files.grouplens.org/datasets/movielens/ml-20m.zip
 unzip ml-20m.zip
 wget http://files.grouplens.org/datasets/movielens/ml-latest-small.zip
 unzip ml-latest-small.zip
+mkdir lastfm
+cd lastfm
+wget http://files.grouplens.org/datasets/hetrec2011/hetrec2011-lastfm-2k.zip
+unzip hetrec2011-lastfm-2k.zip
